b95d9006ea0acd42ad0234989aeae0698f3be73b
jolilphant <102636013+jolilphant@users.noreply.github.com>
2025-10-03 15:29:00 -0600

    Included test for add() function

--- test_code.py
+++ test_code.py
@@ -2,3 +2,20 @@
 Remember that when I grade your code I do not use this same autograder.
 It is meerly here for you to know what your grade is likely to be assuming you followed instructions correctly to pass the tests."""
 
+import pytest
+import random
+
+def test_add():
+  try: 
+    from Calculator_code import add
+    num1 = random.randint(-100,100)
+    num2 = random.randint(-100, 100)
+    result = add(num1, num2)
+    assert result == num1 + num2
+  except SyntaxError as e:
+    print(f"error importing add(): {e}")
+def main():
+  test_add()
+
+if __name__ == "__main__":
+    main()
