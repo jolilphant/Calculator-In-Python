fe566fe10a8265b835d68e579c71113511b24fd6
jolilphant <102636013+jolilphant@users.noreply.github.com>
2025-10-03 13:44:01 -0600

    Create test_code.py

---
+++ test_code.py
@@ -0,0 +1,4 @@
+"""This code is here to test your calculator program so you can make sure it meets all the required specifications of the assignment.
+Remember that when I grade your code I do not use this same autograder.
+It is meerly here for you to know what your grade is likely to be assuming you followed instructions correctly to pass the tests."""
+
